70d7e75e43c6d226b983eaa3255eab593b9c616f
Apurv Pandey <apurv135068@nitp.ac.in>
2016-08-05 04:01:11 +0530

    Initialised variables

--- app/src/main/java/com/example/user/androidids/service/KeystrokeDynamicsService.java
+++ app/src/main/java/com/example/user/androidids/service/KeystrokeDynamicsService.java
@@ -35,5 +35,70 @@ import android.widget.RelativeLayout;
 
 public class KeystrokeDynamicsService extends Activity implements OnTouchListener, OnClickListener, OnFocusChangeListener{
 
+    private String password = "HELLOWORLD";
+    private EditText mEt; // Edit Text boxes
+    private Button mBSpace, mBdone, mBack, mBChange, mNum;
+    private RelativeLayout mLayout, mKLayout;
+    private boolean isEdit = false;
+    private String mUpper = "upper", mLower = "lower";
+    private int w, mWindowWidth;
+    byte[] buffer;
+    long lastpress;
+    long lastrelease;
+    int trainpress;
+    long press;
+    long release;
+    long keyhold;
+    FileOutputStream fos;
+    InputStream fis;
+    BufferedReader br;
+    boolean start=false;
+    String filename="myfile";
+
+    String lastkey;
+    String storage="";
+    Vector<String> text=new Vector<String>();
+    Vector<Long> p=new Vector<Long>();
+    Vector<Long> r=new Vector<Long>();
+    Vector<Long> p2p1=new Vector<Long>();
+    Vector<Long> p2r1=new Vector<Long>();
+    Vector<Long> r2r1=new Vector<Long>();
+    Vector<Long> kh= new Vector<Long>();
+    int noofbs=0;
+    String op;
+
+    /////////////////////////////////////////////////////
+    int NUMBER_OF_INPUTS;
+    int NUMBER_OF_USER_CASES;
+    int NUMBER_OF_LETTERS;
+
+    double[] MEAN;
+    double[] STANDARD_DEVIATION;
+    double[][] USER_CASES;
+    double[][] TEST_CASE;
+    double[] NEW_USE_CASE;
+
+    final int X=3;
+    final int NO_OF_HITS=5;
+    final int DEVIATION_FACTOR=2;
+    double[] hits;
+    double[] deviation;
+    //////////////////////////////////////////////
+
+
+    boolean isTrainPressed=false;
+    private String sL[] = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j",
+            "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w",
+            "x", "y", "z", "OK", "à", "é", "è", "û", "î" };
+    private String cL[] = { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J",
+            "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W",t
+
+            "X", "Y", "Z", "OK", "à", "é", "è", "û", "î" };
+    private String nS[] = { "!", ")", "'", "#", "3", "$", "%", "&", "8", "*",
+            "?", "/", "+", "-", "9", "0", "1", "4", "@", "5", "7", "(", "2",
+            "\"", "6", "_", "=", "]", "[", "<", ">", "|" };
+    private Button mB[] = new Button[27];
+    Button mtrain,mtest;
+
     
 }
