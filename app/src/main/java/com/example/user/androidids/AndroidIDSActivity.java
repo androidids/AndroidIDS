54127adc7dbf0ca10ea0e512638f4f2e015debda
Apurv Pandey <apurv135068@nitp.ac.in>
2016-08-04 23:18:12 +0530

    Refract

--- app/src/main/java/com/example/user/androidids/AndroidIDSActivity.java
+++ app/src/main/java/com/example/user/androidids/AndroidIDSActivity.java
@@ -3,7 +3,7 @@ package com.example.user.androidids;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class AdnroidIDSActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
